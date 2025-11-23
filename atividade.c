a33e462e792d5977fb1d9c9550ee8d506819c36b
FS.Alex <alexsilvaferreira23@gmail.com>
2025-11-22 22:57:43 -0300

    Atividade A1 facul

---
+++ atividade.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i, n;
+    float valor, soma = 0;
+
+    printf("Quantos produtos deseja registrar? ");
+    scanf("%d", &n);
+
+for (i = 1;  i <= n; i++){
+    
+        printf("Preco do produto %d: ", i);
+        scanf("%f", &valor);
+        soma += valor;
+
+    }
+
+    printf("Valor total da compra: %.2f\n",  soma);
+    return 0;
+}
