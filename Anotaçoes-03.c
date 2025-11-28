5f4db3bb3855490d3d4bfa781e659752b57e5106
FS.Alex <alexsilvaferreira23@gmail.com>
2025-11-28 13:56:31 -0300

    Primeiras anotações  e práticas em C

---
+++ Anotaçoes-03.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("Hoje está um \"LINDO\" dia !!!\n");  // CAracterec especial \ é utilizada para retirar o siginificado especial que um caractere possui no caso ("") não é possível adicionar as sendo que elas delimitam uma string.//
+
+return 0;
+
+}
+                                                // Em alguns casos serve parar representar alguns caracteres como://
+                                                // \n(New Line)// 
+                                                // \7(Bell)//
+                                                // \a(Bell)//
+                                                // \b(BackSpace)//
+                                                // \r(Carriage Return)//
+                                                // \t(Tabulação Vertical)//
+                                                // \\(CAracterer[representa a próŕia \])//
+                                                // \'(aspas simples ')//
+                                                // \"(aspas)//
+                                                // \?(ponto de interrogação)
---
+++ Prática-01.c
@@ -0,0 +1,12 @@
+#include  <stdio.h> //bliblioteca Padrão//
+#include <stdlib.h>  //bliblieteca para a função(system(Windos))(getchar(Linux))//
+
+int main(){        // função main e estrutura padrão//
+
+    printf("Hello, world!\n");     // escreva da linguagem //
+
+    getchar();
+
+    return 0;
+
+}
