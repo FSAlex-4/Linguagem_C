536dc7fce986224a69c42d46aa0b412f23ec94bf
FS.Alex <alexsilvaferreira23@gmail.com>
2026-04-22 10:51:08 -0300

    Vetores e neuronios fritando

---
+++ Práticas /modificando_vetor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, n;
+    int vetor[5];
+
+    for ( i = 0; i < 5; i++)
+    {
+        printf("Digite um número: ");
+        scanf("%d", &vetor[i]);
+        }
+    printf("Digite o valor que deseja excluir: ");
+    scanf("%n", &n);
+    for ( i = 0; i < 5; i++)
+    {   if (vetor[i] == n)
+    {
+        
+    }
+    
+            
+            }
+    
+    
+
+
+}
